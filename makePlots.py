e071dca85b84c67f288b424996f82fa846673e15
Zeynep Demiragli <zeynep.demiragli@gmail.com>
2015-07-06 12:00:31 +0200

    adding datacard dump to the macro

--- makePlots.py
+++ makePlots.py
@@ -16,18 +16,71 @@ print "Starting Plotting Be Patient!"
 
 lumi = 19.72
 
-def get_ratio(channel, Variable,added, data, bin):
-    sf = []
-    #Compute all MC except the Zll or Wln
-    added.Add(Variable,-1)
-    #Subtract all background from data
-    data.Add(added,-1)
-    #Divide remaining data with Z
-    data.Divide(Variable)
-    for i in range(bin):
-        i+=1
-        sf.append(data.GetBinContent(i))
-    return sf
+def dump_datacard(channel,yields_dic):
+    nprocess = 0
+    yields_slim = {}
+    for process in yields_dic.keys():
+        nprocess += 1
+        if process not in 'data' and process not in 'signal_higgs':
+            yields_slim[process]=yields_dic[process]
+            
+    datacard = open('datacard_'+channel+'.txt', 'w')
+    datacard.write('')
+    datacard.write( 'imax 1 number of bins \n') 
+    datacard.write( 'jmax '+str(nprocess-2)+' number of processes minus 1 \n') # -1 for data
+    datacard.write( 'kmax * number of nuisance parameters \n')
+    datacard.write( '----------------------------------------------------------------------------------------------------------------------------------------------\n')
+    datacard.write( 'shapes * '+channel+' FAKE \n')
+    datacard.write( '----------------------------------------------------------------------------------------------------------------------------------------------\n')
+    datacard.write('bin                 ' +channel+'\n')
+    datacard.write('observation         ' +str(yields_dic['data']) + '\n')
+    datacard.write( '----------------------------------------------------------------------------------------------------------------------------------------------\n')
+    datacard.write('{0:40s}'.format('bin'))
+    for process in yields_dic.keys():
+        if process not in 'data':
+            datacard.write('{0:20s}'.format(channel))
+    datacard.write('\n')
+    datacard.write('{0:40s}'.format('process'))
+    datacard.write('{0:20s}'.format('signal_higgs'))
+    for process in yields_slim.keys():
+        datacard.write('{0:20s}'.format(process))
+    datacard.write('\n')
+    datacard.write('{0:21s}'.format('process'))
+    datacard.write('{0:20d}'.format(0))
+    for num, process in enumerate(yields_slim.keys()):
+        datacard.write('{0:20d}'.format(num+1))
+    datacard.write('\n')
+    datacard.write('{0:21s}'.format('rate'))
+    datacard.write('{0:20.3f}'.format(yields_dic['signal_higgs']))
+    for process in yields_slim.keys():
+        datacard.write('{0:20.3f}'.format(yields_dic[process]))
+    datacard.write('\n')
+    datacard.write( '----------------------------------------------------------------------------------------------------------------------------------------------\n')
+    datacard.write('{0:20s}'.format('syst_'+channel+'_bkg'))
+    datacard.write('{0:20s}'.format('lnN'))
+    for process in yields_dic.keys():        
+        if process not in 'data':
+            datacard.write('{0:20s}'.format('-'))
+    datacard.write('\n')
+    datacard.write('{0:20s}'.format('syst_'+channel+'_sig'))
+    datacard.write('{0:20s}'.format('lnN'))
+    for process in yields_dic.keys():
+        if process not in 'data':
+            datacard.write('{0:20s}'.format('-'))
+    datacard.write('\n')
+    datacard.write('{0:20s}'.format('syst_Zjets_norm'))
+    datacard.write('{0:20s}'.format('lnU'))
+    for process in yields_dic.keys():
+        if process not in 'data':
+            datacard.write('{0:20s}'.format('-'))
+    datacard.write('\n') 
+    datacard.write('{0:20s}'.format('syst_WJets_norm'))
+    datacard.write('{0:20s}'.format('lnU'))
+    for process in yields_dic.keys():
+        if process not in 'data':
+            datacard.write('{0:20s}'.format('-'))
+    datacard.write('\n') 
+    datacard.close()
 
 def plot_ratio(pull,data,mc,bin,xlabel):
 
@@ -96,13 +149,16 @@ def plot_stack(channel, name,var, bin, low, high, ylabel, xlabel, setLog = False
         var = update_var(channel,var)
 
     folder = 'test'
-    yields = {}
+    yield_Zll = {}
+    yield_dic = {}
+    yield_Wln = {}
+    yield_signal = {}
     stack = THStack('a', 'a')
     added = TH1D('a', 'a',bin,low,high)
     added.Sumw2()
 
     Variables = {}    
-    cut_standard= build_selection(channel,200)
+    cut_standard= build_selection(channel,500)
     print "Channel is: ", channel, " variable is: ", var, " Selection is: ", cut_standard,"\n"
 
     reordered_physics_processes = []
@@ -111,7 +167,7 @@ def plot_stack(channel, name,var, bin, low, high, ylabel, xlabel, setLog = False
  
     for Type in reordered_physics_processes:
         # Create the Histograms
-        histName = Type+name+channel
+        histName = Type+'_'+name+'_'+channel
         Variables[Type] = TH1F(histName, histName, bin, low, high)
         Variables[Type].Sumw2()
 
@@ -126,22 +182,43 @@ def plot_stack(channel, name,var, bin, low, high, ylabel, xlabel, setLog = False
             stack.Add(Variables[Type],"hist")
             added.Add(Variables[Type])
 
-        #if Type.startswith('signal'):
-        #    Variables[Type].SetLineColor(1)
-        #    Variables[Type].SetLineWidth(3)
-        #    Variables[Type].SetLineStyle(8)
-        #    Trees[Type].Draw(var + " >> " + histName,  "(" + cut_standard + " ) *"+weight   , "goff")
-        #    Variables[Type].Scale(float(lumi)*1000)
+        if Type.startswith('signal_higgs'):
+            Variables[Type].SetLineColor(1)
+            Variables[Type].SetLineWidth(3)
+            Variables[Type].SetLineStyle(8)
+            makeTrees(Type,channel).Draw(var + " >> " + histName,"(" + cut_standard + ")* weight","goff")
+            Variables[Type].Scale(float(lumi)*1000)
                         
         if Type.startswith("data"):
             Variables[Type].SetMarkerStyle(20)
             makeTrees(Type,channel).Draw(var + " >> " + histName,  "(" + cut_standard + " ) * weight"   , "goff")
         
-        yields[(Type, channel)] = Variables[Type].Integral()
+        yield_dic[Type] = round(Variables[Type].Integral(),3)
+
+        #if channel in 'Zll':
+        #    yield_Zll[Type] = round(Variables[Type].Integral(),3)
+        #    print Type, yield_Zll[Type]
+        #if channel in 'Wln':
+        #    yield_Wln[Type] = Variables[Type].Integral()
+        #if channel in 'signal':
+        #    yield_signal[Type] = Variables[Type].Integral()
+
         #print "Type is: ",Type, " Channel is: ", channel, " Integral is: ",Variables[Type].Integral()
-        
+
+
+        #Variables[Type].SetName(Type+channel)
+        #Variables[Type].SetTitle(Type+channel)
+        #Variables[Type].Write()
+
+
+    dump_datacard(channel,yield_dic)
+
+    added.Write()
+
     legend = TLegend(.60,.60,.92,.92)
     for process in  ordered_physics_processes:
+        Variables[process].SetTitle(process)
+        Variables[process].Write()
         if process is not 'data':
             legend . AddEntry(Variables[process],physics_processes[process]['label'] , "f")
         else:
@@ -165,7 +242,7 @@ def plot_stack(channel, name,var, bin, low, high, ylabel, xlabel, setLog = False
     stack.GetXaxis().SetTitle('')
 
     Variables['data'].Draw("Esame")
-    #Variables['signal'].Draw("same")
+    Variables['signal_higgs'].Draw("same")
     
     legend.SetShadowColor(0);
     legend.SetFillColor(0);
@@ -194,13 +271,13 @@ def plot_stack(channel, name,var, bin, low, high, ylabel, xlabel, setLog = False
 
     c4.SaveAs(folder+'/Histo_' + name + '_'+channel+'.pdf')
 
+    #f.Write()
+    f.Close()
+
     del Variables
     del var
     c4.IsA().Destructor( c4 )
     stack.IsA().Destructor( stack )
-
-
-
 
 arguments = {}
 #                = [var, bin, low, high, yaxis, xaxis, setLog]
@@ -211,13 +288,17 @@ arguments['jetpt']  = ['jetpt','jet1.pt()',17,150,1000,'Events/50 GeV','Leading 
 arguments['njets']  = ['njets','njets',3,1,4,'Events','Number of Jets',True]
 
 channel_list  = ['signal','Wln','Zll']
-#channel_list  = ['Wln']
+#channel_list  = ['Zll']
 #variable_list = ['met','jetpt','njets','metRaw','genmet']
 processes     = []
 
 variable_list = ['met']
 
 for channel in channel_list:
+    f =TFile("monojet_"+channel+".root","RECREATE")
+    f.cd('..')
+    f.mkdir(channel)
+    f.cd(channel)
     for var in variable_list:
         arguments[var].insert(0,channel)
         print  arguments[var]
